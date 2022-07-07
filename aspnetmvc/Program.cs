660a0f61d4f98e1fcfe251c0e67ac1e05c4c2467
Paulo <paulonrss@gmail.com>
2022-07-07 02:15:56 -0300

    Project Create

--- aspnetmvc/Program.cs
+++ aspnetmvc/Program.cs
@@ -14,6 +14,7 @@ namespace aspnetmvc
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("aaaa");
             CreateWebHostBuilder(args).Build().Run();
         }
 
