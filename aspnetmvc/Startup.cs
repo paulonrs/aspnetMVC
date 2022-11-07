ef79b3072a14d2a84bd8921d95a55566ec4173dc
Paulo Roberto <paulo.roberto@salesoft.com.br>
2022-11-07 13:38:08 -0300

    adicao do builder

--- aspnetmvc/Startup.cs
+++ aspnetmvc/Startup.cs
@@ -36,7 +36,8 @@ namespace aspnetmvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<aspnetmvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("aspnetmvcContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("aspnetmvcContext"), 
+                    buider => buider.MigrationsAssembly("aspnetmvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
